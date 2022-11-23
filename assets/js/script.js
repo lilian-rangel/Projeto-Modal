4122070489b21ed92c9087f87cab3cb12fd2bc9c
lilian-rangel <lilianrm.nutri@gmail.com>
2022-11-23 10:54:35 -0300

    add eventListener js/open-close modal css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 
-const modalBtn = document.querySelector('.modal-btn');
-const modal = document.querySelector('.modal-overlay');
-const closeBtn = document.querySelector('.close-btn');
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector(".close-btn");
 
-modalBtn.addEventListener('clic', function (){
-
-})
-
-closeBtn.addEventListener('clic', function (){
-
-})
+modalBtn.addEventListener("click", function () {
+    modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function () {
+    modal.classList.remove("open-modal");
+});
